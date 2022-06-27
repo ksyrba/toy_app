55cf5bdfa75afbc64c1555bd7691a1fb24447a45
Ubuntu <ksyrbaba0812@gmail.com>
2022-06-27 13:49:29 +0000

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :microposts
   
   validates :name, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true 
 end
